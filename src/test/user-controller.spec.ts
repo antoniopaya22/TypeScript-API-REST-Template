93e81bf0d38e0a1ccf9428d288e19e1a6b5806cc
antonioalfa22 <antonioalfa22@gmail.com>
2020-02-25 12:15:15 +0100

    Updated test

--- src/test/user-controller.spec.ts
+++ src/test/user-controller.spec.ts
@@ -5,7 +5,6 @@ import { UserController } from '../controllers/user-controller';
 describe('First test', 
   () => { 
     it('should return true', () => { 
-      const userController = new UserController();
       expect(true).to.equal(true); 
   }); 
 });
